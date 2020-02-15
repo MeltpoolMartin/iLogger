d4afb742b684cb641ca2c6f42513c3f083446a6c
Martin Wachter <martinwachter@gmx.net>
2020-02-15 22:16:54 +0100

    creating repo for iLogger

---
+++ src/iLogger/iLogger/iLogger.h
@@ -0,0 +1,18 @@
+//
+//  iLogger-h.h
+//  iLogger
+//
+//  Created by Martin on 15.02.20.
+//  Copyright © 2020 Martin. All rights reserved.
+//
+
+#ifndef iLogger_h_h
+#define iLogger_h_h
+
+
+#endif /* iLogger_h_h */
+
+class iLogger {
+    
+    
+};
---
+++ src/iLogger/iLogger/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  iLogger
+//
+//  Created by Martin on 15.02.20.
+//  Copyright © 2020 Martin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
